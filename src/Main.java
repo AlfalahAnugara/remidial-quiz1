f4e4639b4e16515fe93229ebeca3bc1fc9b3708f
MSI Notebook <MSI Notebook@DESKTOP-2D6BNE3>
2018-11-04 00:38:12 +0700

    Pembuatan class main dan project sudah selesai

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,46 @@ public class Main {
     
     static Scanner gc = new Scanner (System.in) ;
     public static void main (String [] args) {
+        
         TomClancys tc = new TomClancys() ;
-            System.out.println("Tom Clancy's The Division") ;
-            System.out.println("Tom Clancy's Rainbow Six Siege") ;
-            System.out.println("Tom Clancy's Splinter Cell") ;
+        tc.setPegiAge(18) ;
+            System.out.println("1.) Tom Clancy's The Division") ;
+            System.out.println("2.) Tom Clancy's Rainbow Six Siege") ;
+            System.out.println("3.) Tom Clancy's Splinter Cell") ;
             System.out.print("Pilihan") ;
+            int aaa = gc.nextInt() ;
+            if (aaa == 1) {
+                tc.setJudul ("Tom Clancy's The Division") ;
+            } else if (aaa == 2) {
+                tc.setJudul ("Tom Clancy's Rainbow Six Siege") ;
+            } else if (aaa == 3) {
+                tc.setJudul ("Tom Clancy's Splinter Cell") ;
+            } else {
+                tc.setJudul (" ") ;  
+            }
+            
+        CallOfDuty cod = new CallOfDuty() ;
+        cod.setPegiAge(12) ;
+            System.out.println("1.) Call Of Duty Black Ops 4") ;
+            System.out.println("2.) Call Of Duty Advanced Warfare") ;
+            System.out.println("3.) Call Of Duty Ghost") ;
+            System.out.print("Pilihan : ") ;
+            int bbb = gc.nextInt() ;
+            if (bbb == 1) {
+                cod.setJudul ("Call Of Duty Black Ops4") ;
+            } else if (bbb == 2) {
+                cod.setJudul ("Call Of Duty Advanced Warfare") ;
+            } else if (bbb == 3) {
+                cod.setJudul ("Call Of Duty Ghost") ;
+            }else {
+                cod.setJudul(" ") ;
+            }
+            
+            Games G = new Games () ;
+            G.setTomclancys(tc) ;
+            G.setCallofduty(cod) ;
+            
+            G.info () ;
             
     }
     
