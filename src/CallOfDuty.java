fbb30f79d17f82c1586016dcae1a90ef378c6a55
MSI Notebook <MSI Notebook@DESKTOP-2D6BNE3>
2018-11-04 00:00:06 +0700

    membuat konstruktor dan getter dan setter menggunakan fungsi insert code pada netbeans

--- src/CallOfDuty.java
+++ src/CallOfDuty.java
@@ -13,4 +13,33 @@ public class CallOfDuty {
     private String judul ;
     private int harga ;
     private int pegiAge ;
+
+    public CallOfDuty() {
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public int getPegiAge() {
+        return pegiAge;
+    }
+
+    public void setPegiAge(int pegiAge) {
+        this.pegiAge = pegiAge;
+    }
+    
+    
 }
